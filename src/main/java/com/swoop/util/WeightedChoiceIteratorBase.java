ff14c79c985873e28e41090e9638329cd3d13b1d
James Echmalian <ech@ech.net>
2013-05-07 18:54:40 -0400

    generalize WeightedChoiceIteratorBase to adapt any iterable type

--- src/main/java/com/swoop/util/WeightedChoiceIteratorBase.java
+++ src/main/java/com/swoop/util/WeightedChoiceIteratorBase.java
@@ -1,42 +1,86 @@
 package com.swoop.util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+
 public abstract class WeightedChoiceIteratorBase<V>
 	implements WeightedChoiceIterator<V>
 {
-	private V[] choices;
-	private int index;
+	private Iterable<V> choices;
+	private Iterator<V> iterator;
+	private V current;
 
+	/**
+	 * Constructor.
+	 */
 	public WeightedChoiceIteratorBase()
 	{
+		this((Iterable<V>)Collections.EMPTY_LIST);
 	}
 
+	/**
+	 * Constructor.
+	 * @param choices   the initial list of choices, expressed as an array
+	 */
 	public WeightedChoiceIteratorBase(V[] choices)
 	{
 		reset(choices);
 	}
 
+	/**
+	 * Constructor.
+	 * @param choices   the initial list of choices, expressed as an iterable collection
+	 */
+	public WeightedChoiceIteratorBase(Iterable<V> choices)
+	{
+		reset(choices);
+	}
+
+	/**
+	 * Reset to the beginning of a new list of choices.
+	 * @param choices   the list of choices, expressed as an array
+	 */
 	public void reset(V[] choices)
 	{
+		reset(Arrays.asList(choices));
+	}
+
+	/**
+	 * Reset to the beginning of a new list of choices.
+	 * @param choices   the list of choices, expressed as an iterable collection
+	 */
+	public void reset(Iterable<V> choices)
+	{
 		this.choices = choices;
-		this.index = 0;
+		reset();
+	}
+
+	/**
+	 * Reset to the beginning of the current list of choices.
+	 */
+	public void reset()
+	{
+		this.iterator = choices.iterator();
+		this.current = null;
 	}
 
 	@Override
 	public boolean hasNext()
 	{
-		return index < choices.length;
+		return iterator.hasNext();
 	}
 
 	@Override
 	public double nextWeight()
 	{
-		return getWeight(choices[index++]);
+		return getWeight(current = iterator.next());
 	}
 
 	@Override
 	public V getCurrentValue()
 	{
-		return choices[index - 1];
+		return current;
 	}
 
 	abstract protected double getWeight(V value);
