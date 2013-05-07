47657daeeb873d28dcfdb73f5261a108141412ad
James Echmalian <ech@ech.net>
2013-05-07 18:44:23 -0400

    implement multiple random weighted selection

--- src/main/java/com/swoop/util/Randomizer.java
+++ src/main/java/com/swoop/util/Randomizer.java
@@ -1,5 +1,10 @@
 package com.swoop.util;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Randomizer
 {
 	private RandomNumberGenerator rng;
@@ -24,17 +29,17 @@ public class Randomizer
 
 	/**
 	 * Make a selection among weighted choices.
-	 * @param weights
-	 * @return  the index of the selected item, or -1 if no item was selected
+	 * @param choices  a weighted view of the choices
+	 * @return  the selected item, or null if no item was selected
 	 */
 	public <V> V select(WeightedChoiceIterator<V> choices)
 	{
 		double runningTotalWeight = 0.0;
 		V candidate = null;
 		while (choices.hasNext()) {
-			double prevTotalWeight = runningTotalWeight;
 			double weight = choices.nextWeight();
 			if (weight > 0.0) {
+				double prevTotalWeight = runningTotalWeight;
 				runningTotalWeight += weight;
 				if ((rng.nextDouble() * runningTotalWeight) >= prevTotalWeight) {
 					candidate = choices.getCurrentValue();
@@ -42,5 +47,70 @@ public class Randomizer
 			}
 		}
 		return candidate;
+	}
+
+	/**
+	 * Make a top N selection among weighted choices.
+	 * @param choices  a weighted view of the choices
+	 * @param n   the maximum number of items to return
+	 * @return a non-null list of selected items, the size of which never exceeds n
+	 */
+	public <V> List<V> select(WeightedChoiceIterator<V> choices, int n)
+	{
+		double runningTotalWeight = 0.0;
+		LinkedList<ChoiceNode<V>> top = null;
+		while (choices.hasNext()) {
+			double weight = choices.nextWeight();
+			if (weight > 0.0) {
+				if (top == null) {
+					top = new LinkedList<ChoiceNode<V>>();
+				}
+				double prevTotalWeight = runningTotalWeight;
+				runningTotalWeight += weight;
+				double workingTotalWeight = runningTotalWeight;
+				ListIterator<ChoiceNode<V>> candidates = top.listIterator(); 
+				boolean inserted = false;
+				while (candidates.hasNext()) {
+					ChoiceNode<V> candidate = candidates.next();
+					if ((rng.nextDouble() * workingTotalWeight) >= prevTotalWeight) {
+						candidates.set(new ChoiceNode(choices.getCurrentValue(), weight));
+						candidates.add(candidate);
+						if (top.size() > n) {
+							top.removeLast();
+						}
+						inserted = true;
+						break;
+					}
+					workingTotalWeight -= candidate.weight;
+					prevTotalWeight -= candidate.weight;
+				}
+				if (!inserted && top.size() < n) {
+					top.add(new ChoiceNode(choices.getCurrentValue(), weight));
+				}
+			}
+		}
+		return toValueList(top);
+	}
+
+	private static class ChoiceNode<V>
+	{
+		V value;
+		double weight;
+
+		ChoiceNode(V value, double weight) {
+			this.value = value;
+			this.weight = weight;
+		}
+	}
+
+	private static <V> List<V> toValueList(List<ChoiceNode<V>> nodeList)
+	{
+		List<V> valueList = new ArrayList<V>(nodeList == null ? 0 : nodeList.size());
+		if (nodeList != null) {
+			for (ChoiceNode<V> node : nodeList) {
+				valueList.add(node.value);
+			}
+		}
+		return valueList;
 	}
 }
--- src/test/java/com/swoop/util/RandomizerTest.java
+++ src/test/java/com/swoop/util/RandomizerTest.java
@@ -7,31 +7,31 @@ import static org.junit.Assert.*;
 public class RandomizerTest
 {
 	@Test
-	public void testSelectAmongNone() throws Exception
+	public void testSelectOneAmongNone() throws Exception
 	{
 		assertNull(new Randomizer().select(new WeightedChoiceAdapter(new double[0])));
 	}
 
 	@Test
-	public void testSelectAmongZero() throws Exception
+	public void testSelectOneAmongZero() throws Exception
 	{
 		assertNull(new Randomizer().select(new WeightedChoiceAdapter(new double[] { 0 })));
 	}
 
 	@Test
-	public void testSelectAmongZeroes() throws Exception
+	public void testSelectOneAmongZeroes() throws Exception
 	{
 		assertNull(new Randomizer().select(new WeightedChoiceAdapter(new double[] { 0, 0 })));
 	}
 
 	@Test
-	public void testSelectAmongOne() throws Exception
+	public void testSelectOneOfOne() throws Exception
 	{
 		assertEquals(0, new Randomizer().select(new WeightedChoiceAdapter(new double[] { 10 })).intValue());
 	}
 
 	@Test
-	public void testSelectAmongMany00() throws Exception
+	public void testSelectOneAmongMany00() throws Exception
 	{
 		Randomizer rand = new Randomizer(new RiggedRandomNumberGenerator(0));
 		assertEquals(0, rand.select(new WeightedChoiceAdapter(new double[] {
@@ -40,7 +40,7 @@ public class RandomizerTest
 	}
 
 	@Test
-	public void testSelectAmongMany05() throws Exception
+	public void testSelectOneAmongMany05() throws Exception
 	{
 		Randomizer rand = new Randomizer(new RiggedRandomNumberGenerator(0.5));
 		assertEquals(3, rand.select(new WeightedChoiceAdapter(new double[] {
@@ -49,7 +49,7 @@ public class RandomizerTest
 	}
 
 	@Test
-	public void testSelectAmongMany10() throws Exception
+	public void testSelectOneAmongMany10() throws Exception
 	{
 		Randomizer rand = new Randomizer(new RiggedRandomNumberGenerator(1.0));
 		assertEquals(4, rand.select(new WeightedChoiceAdapter(new double[] {
@@ -58,7 +58,7 @@ public class RandomizerTest
 	}
 
 	@Test
-	public void testRandomnessOfSelection() throws Exception
+	public void testRandomnessOfSingleSelection() throws Exception
 	{
 		double[] weights = { 1.0, 4.0, 16.0, 64.0, 256.0 };
 		boolean[] covered = new boolean[weights.length];
@@ -70,6 +70,71 @@ public class RandomizerTest
 				covered[sel] = true;
 				ncovered += 1;
 				if (ncovered == weights.length) {
+					return;
+				}
+			}
+		}
+		fail("should not be reached");
+	}
+
+	@Test
+	public void testSelectSomeAmongNone() throws Exception
+	{
+		List<Integer> result = new Randomizer().select(new WeightedChoiceAdapter(new double[0]), 5);
+		assertNotNull(result);
+		assertEquals(0, result.size());
+	}
+
+	@Test
+	public void testSelectSomeAmongZero() throws Exception
+	{
+		List<Integer> result = new Randomizer().select(new WeightedChoiceAdapter(new double[] { 0 }), 5);
+		assertNotNull(result);
+		assertEquals(0, result.size());
+	}
+
+	@Test
+	public void testSelectSomeAmongZeroes() throws Exception
+	{
+		List<Integer> result = new Randomizer().select(new WeightedChoiceAdapter(new double[] { 0, 0 }), 5);
+		assertNotNull(result);
+		assertEquals(0, result.size());
+	}
+
+	@Test
+	public void testTakeWhatYouCanGet() throws Exception
+	{
+		List<Integer> result = new Randomizer().select(new WeightedChoiceAdapter(new double[] { 10 }), 5);
+		assertNotNull(result);
+		assertEquals(1, result.size());
+	}
+
+	@Test
+	public void testTakeWhatYouCanGet2() throws Exception
+	{
+		List<Integer> result = new Randomizer().select(new WeightedChoiceAdapter(new double[] { 5, 10 }), 5);
+		assertNotNull(result);
+		assertEquals(2, result.size());
+		assertTrue(result.get(0).intValue() != result.get(1).intValue());
+	}
+
+	@Test
+	public void testRandomnessOfMultiSelection() throws Exception
+	{
+		final double[] weights = { 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0 };
+		final int NVALUES = (7*6*5)/(1*2*3); // C(7,3)
+		final boolean[] covered = new boolean[16 + 32 + 64 + 1];
+		int ncovered = 0;
+		Randomizer rand = new Randomizer();
+		for (int trials = 0; trials < 100000000; ++trials) {
+			int sel = 0;
+			for (int s : rand.select(new WeightedChoiceAdapter(weights), 3)) {
+				sel += 1<<s;
+			}
+			if (!covered[sel]) {
+				covered[sel] = true;
+				ncovered += 1;
+				if (ncovered == NVALUES) {
 					return;
 				}
 			}
