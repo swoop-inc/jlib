de039aa35ce0d0f79a9788675a30ee17adbe4cdf
Sebastian Gavarini <sebastian.gavarini@natyo.com>
2013-07-02 13:02:00 +0200

    Multi set command updated

--- src/main/java/com/swoop/data/redis/MultiSetRedisCommand.java
+++ src/main/java/com/swoop/data/redis/MultiSetRedisCommand.java
@@ -11,9 +11,14 @@ abstract public class MultiSetRedisCommand implements RedisCommand<String>
 	@Override
 	public String execute(SwoopBinaryJedisCommands jedis) throws JedisException, IOException
 	{
-		
-		return jedis.mset(getBinaryKeyValues());
+		return jedis.mset(getBinaryKeyAndValues());
 	}
-
-	abstract protected byte[] getBinaryKeyValues() throws IOException;
+	
+	/**
+	 * byte[] of mixed keys and values, even position keys and odd position values.
+	 * 
+	 * @return
+	 * @throws IOException
+	 */
+	abstract protected byte[][] getBinaryKeyAndValues() throws IOException;
 }
