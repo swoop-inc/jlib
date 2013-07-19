a28a4f7f9a3face311bc4874d05bbef00d8221c5
James Echmalian <ech@ech.net>
2013-07-19 15:51:47 -0400

    Add initial redis integration test

---
+++ redis-integration/redis/README.md
@@ -0,0 +1,1 @@
+Directory that bin/redis_start uses to store redis data and logs.
---
+++ redis-integration/src/test/java/com/swoop/data/redis/RedisTestCase.java
@@ -0,0 +1,13 @@
+package com.swoop.data.redis;
+
+public class RedisTestCase
+{
+	static final int REDIS_PORT = 6380;   // must match that in ${project-root}/bin/redis_start
+
+	public static RedisConnector createRedisConnector() throws Exception
+	{
+		RedisConnectorConfig config = new RedisConnectorConfig();
+		config.setPort(REDIS_PORT);
+		return new DefaultRedisConnector(config);
+	}
+}
