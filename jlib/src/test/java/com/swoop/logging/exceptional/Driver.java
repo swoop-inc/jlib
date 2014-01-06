35af303d5a06a746fade38b6577de60b912cd4b5
James Echmalian <ech@ech.net>
2014-01-06 11:18:01 -0500

    Add utility for exceptional.io connector testing.

---
+++ jlib/src/test/java/com/swoop/logging/exceptional/Driver.java
@@ -0,0 +1,44 @@
+package com.swoop.logging.exceptional;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class Driver
+{
+	ExceptionalConnector connector;
+
+	public static void main(String[] args)
+	{
+		Driver driver = new Driver();
+		driver.postException();
+		driver.postMessage();
+	}
+
+	public Driver()
+	{
+		this.connector = new ExceptionalConnectorBuilder()
+			.setApiKey(System.getProperty("exceptional.api.key"))
+			.setExecutorService(Executors.newSingleThreadExecutor())
+			.setReportFormatter(new ExceptionalReportFormatter())
+			.setHttpClient(HttpClientBuilder.create().build())
+			.setLog(System.out)
+			.build();
+	}
+
+	private void postException()
+	{
+		try {
+			System.out.println((new Object[0])[1]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			connector.post(new Exception(e));
+		}
+	}
+
+	private void postMessage()
+	{
+		connector.post(new ExceptionalReportBuilder("this is a test").build());
+	}
+}
